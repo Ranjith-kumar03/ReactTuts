632697f246e2b72265c4d5e0a345664b0a66d312
unknown <ranjithkumar03.1980@gmail.com>
2022-05-17 13:03:47 +0530

    chapter10

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,19 +3,21 @@ import './App.css';
 //import Greet from'./components/Greet/Greet'
 import {Greet} from'./components/Greet/Greet'
 import Welcome from'./components/Greet/Welcome'
+import Message from'./components/Greet/Message'
 function App() {
   
   return (
     <div className="App">
       <header className="App-header">
        <div>Hello world from crap</div>
-       <Greet name="muskal"><p>Hi muskal we Greet You</p></Greet>
+       {/* <Greet name="muskal"><p>Hi muskal we Greet You</p></Greet>
        <Greet name="paskal"/>
        <Greet name="qaskal"/>
 
        <Welcome name="muskal"/>
        <Welcome name="paskal"/>
-       <Welcome name="qaskal"/>
+       <Welcome name="qaskal"/> */}
+       <Message />
       </header>
     </div>
   );
