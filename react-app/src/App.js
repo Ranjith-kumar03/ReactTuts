5ba3782d3abb3f0b8f8a131ffd21d8de017130f9
unknown <ranjithkumar03.1980@gmail.com>
2022-05-17 11:33:59 +0530

    chapter 5

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
-
+//import Greet from'./components/Greet/Greet'
+import {Greet} from'./components/Greet/Greet'
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
        <div>Hello world from crap</div>
+       <Greet/>
       </header>
     </div>
   );
